b378ba34c0fa2ae7e20dd854e3a46b48d1a12545
Zachary Lee <w.zachary.lee@gmail.com>
2016-06-27 14:38:00 -0500

    yey

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {observer} from 'mobx-react';
 
+import DevTool from 'mobx-react-devtools';
+
 @observer
 export default class App extends React.Component {
-  componentDidMount() {
-
-  }
   render() {
     return (
       <div>
+        <DevTool />
         <h1>Hello from App Component!</h1>
         {this.props.children}
       </div>
